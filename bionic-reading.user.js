b2c79c5ffedfd7ccc4842c56cde54d1b7281fb05
itorr <itorrrrrr@me.com>
2022-05-24 18:37:38 +0800

    #23 跳过可编辑元素;#22 Opacity 支持;#25 symbol Mode; #19 autoBionic config;#12 maxLength config;#7 skip Words config

--- bionic-reading.user.js
+++ bionic-reading.user.js
@@ -1,15 +1,66 @@
 // ==UserScript==
-// @name         英文前部加粗
-// @namespace    https://github.com/itorr/bionic-reading.user.js
-// @version      0.8.4
-// @description  网页英文前部加粗脚本 Ctrl + B / ⌘ + B 开启关闭
-// @author       itorr
-// @match        *://*/*
-// @grant        none
-// @license      MIT
-// @run-at       document-idle
-// @supportURL   https://github.com/itorr/bionic-reading.user.js/issues
+// @name              Bionic Reading / ⌘ + B
+// @name:zh           英文前部加粗 / ⌘ + B
+// @namespace         https://github.com/itorr/bionic-reading.user.js
+// @version           0.8.4
+// @description       Bionic Reading User Script Ctrl + B / ⌘ + B
+// @description:zh    网页英文前部加粗脚本 Ctrl + B / ⌘ + B 开启关闭
+// @icon              data:image/vnd.microsoft.icon;base64,AAABAAEAICACAAEAAQAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABvb28Ab29vAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/////////////////////8AAfH/AABx/wAAMf8AADH/D/gR/w/8Ef8P/BH/D/wR/w/8Ef8P+DH/AAAx/wAAcf8AAPD/AABwfw/wMA8P+DEPD/gxjw/4P/8P8D//AAA//wAAf/8AAP//AAP////////////////////////////8=
+// @author            itorr
+// @match             *://*/*
+// @exclude           /\.(js|java|c|cpp|h|py|css|less|scss|json|yaml|yml|xml)(?:\?.+)$/
+// @license           MIT
+// @run-at            document-end
+// @supportURL        https://github.com/itorr/bionic-reading.user.js/issues
+// @grant             GM.registerMenuCommand
+// @grant             GM.unregisterMenuCommand
+// @grant             GM_registerMenuCommand
+// @grant             GM_unregisterMenuCommand
+// @grant             GM.getValue
+// @grant             GM_getValue
+// @grant             GM.setValue
+// @grant             GM_setValue
 // ==/UserScript==
+
+let base = typeof GM === 'object' ? GM : {
+    getValue: typeof GM_getValue === 'function' ? GM_getValue : _=>{},
+    setValue: typeof GM_setValue === 'function' ? GM_setValue : _=>{},
+    registerMenuCommand : typeof GM_registerMenuCommand === 'function' ? GM_registerMenuCommand : _=>{},
+    registerMenuCommand : typeof GM_unregisterMenuCommand === 'function' ? GM_unregisterMenuCommand : _=>{},
+}
+
+
+const defaultConfig = {
+    autoBionic: true,
+    skipLinks: false,
+    skipWords: false,
+    scale: 0.5,
+    maxBionicLength: null,
+    opacity: 1,
+    // saccade: 1, // 1 - ~
+    symbolMode: false,
+    excludeWords:['is','and','as','if','the','of','to','be','for','this'],
+};
+
+
+const config = await (async _=>{
+    const _config = await base.getValue('config');
+    if(!_config) return defaultConfig;
+
+    for(let key in defaultConfig){
+        if(_config[key] === undefined) _config[key] = defaultConfig[key];
+    }
+
+    return _config;
+})();
+
+try{
+    await base.setValue('config',config);
+}catch(e){
+
+}
+
+console.log(JSON.stringify(config,0,2))
 
 let isBionic = false;
 
@@ -30,7 +81,7 @@ const styleEl = document.createElement('style');
 styleEl.innerHTML = `
 bbb{
     font-weight:bold;
-    opacity: 1;
+    opacity: ${config.opacity};
 }
 html[data-site="greasyfork"] a bionic{
     pointer-events: none;
@@ -67,7 +118,13 @@ const gather = el=>{
             textEls.push(el);
         }else if(el.childNodes){
             if(excludeNodeNames.includes(el.nodeName.toLowerCase())) return;
-            if(el.getAttribute && el.getAttribute('class') && excludeClassesRegexi.test(el.getAttribute('class'))) return;
+            if(el.getAttribute){
+                if(el.getAttribute('class') && excludeClassesRegexi.test(el.getAttribute('class'))) return;
+
+                // 跳过所有可编辑元素
+                if(el.getAttribute('contentEditable') === 'true') return;
+            }
+
             textEls = textEls.concat(gather(el))
         }
     })
@@ -76,7 +133,23 @@ const gather = el=>{
 
 const engRegex  = /[a-zA-Z][a-z]+/;
 const engRegexg = new RegExp(engRegex,'g');
-let replaceTextByEl = el=>{
+const getHalfLength = word=>{
+
+    let halfLength;
+    if(/ing$/.test(word)){
+        halfLength = word.length - 3;
+    }else if(word.length<5){
+        halfLength = Math.floor(word.length * config.scale);
+    }else{
+        halfLength = Math.ceil(word.length * config.scale);
+    }
+
+    if(config.maxBionicLength){
+        halfLength = Math.min(halfLength, config.maxBionicLength)
+    }
+    return halfLength;
+}
+const replaceTextByEl = el=>{
     const text = el.data;
     if(!engRegex.test(text))return;
 
@@ -84,15 +157,10 @@ let replaceTextByEl = el=>{
         const spanEl = document.createElement('bionic');
         spanEl.isEnB = true;
         spanEl.innerHTML = enCodeHTML(text).replace(engRegexg,word=>{
-            let halfLength;
-            if(/ing$/.test(word)){
-                halfLength = word.length - 3;
-            }else if(word.length<5){
-                halfLength = Math.floor(word.length/2);
-            }else{
-                halfLength = Math.ceil(word.length/2);
+            if(config.skipWords){
+                if(config.excludeWords.includes(word)) return word;
             }
-    
+            const halfLength = getHalfLength(word);
             return '<bbb>'+word.substr(0,halfLength)+'</bbb>'+word.substr(halfLength)
         })
         spanEl.originEl = el;
@@ -103,32 +171,27 @@ let replaceTextByEl = el=>{
     el.remove();
 };
 
-//     replaceTextByEl = el=>{
-//         el.data = el.data.replace(engRegexg,word=>{
-//             let halfLength;
-//             if(/ing$/.test(word)){
-//                 halfLength = word.length - 3;
-//             }else if(word.length<5){
-//                 halfLength = Math.floor(word.length/2);
-//             }else{
-//                 halfLength = Math.ceil(word.length/2);
-//             }
-//             const a = word.substr(0,halfLength).
-//                 replace(/[a-z]/g,w=>'\uD835' + String.fromCharCode(w.charCodeAt(0)+56717)).
-//                 replace(/[A-Z]/g,w=>'\uD835' + String.fromCharCode(w.charCodeAt(0)+56723));
-//             const b = word.substr(halfLength).
-//                 replace(/[a-z]/g,w=> String.fromCharCode(55349,w.charCodeAt(0)+56665)).
-//                 replace(/[A-Z]/g,w=> String.fromCharCode(55349,w.charCodeAt(0)+56671));
-//             return a + b;
-//         })
-//     }
+const replaceTextSymbolModeByEl = el=>{
+    el.data = el.data.replace(engRegexg,word=>{
+        const halfLength = getHalfLength(word);
+        const a = word.substr(0,halfLength).
+            replace(/[a-z]/g,w=>String.fromCharCode(55349,w.charCodeAt(0)+56717)).
+            replace(/[A-Z]/g,w=>String.fromCharCode(55349,w.charCodeAt(0)+56723));
+        const b = word.substr(halfLength).
+            replace(/[a-z]/g,w=> String.fromCharCode(55349,w.charCodeAt(0)+56665)).
+            replace(/[A-Z]/g,w=> String.fromCharCode(55349,w.charCodeAt(0)+56671));
+        return a + b;
+    })
+}
 
 const bionic = _=>{
     const textEls = gather(body);
 
     isBionic = true;
 
-    textEls.forEach(replaceTextByEl);
+    const replaceFunc = config.symbolMode ? replaceTextSymbolModeByEl : replaceTextByEl;
+
+    textEls.forEach(replaceFunc);
     document.head.appendChild(styleEl);
 }
 
@@ -161,8 +224,10 @@ if(window.MutationObserver){
     document.addEventListener('DOMNodeInserted',listenerFunc);
 }
 
-
-window.addEventListener('load',bionic);
+if(config.autoBionic){ // auto Bionic
+    console.log(config.autoBionic)
+    window.addEventListener('load',bionic);
+}
 // document.addEventListener('click',listenerFunc);
 
 
@@ -189,7 +254,7 @@ const redo = _=>{
 
         if(!replaceEl) return;
 
-        
+
         el.after(replaceEl);
         el.remove();
     })
@@ -210,5 +275,11 @@ document.addEventListener('keydown',e=>{
         }
     }
 })
+
+
+// let id = base.registerMenuCommand ('Setting', function(){
+//     // 配置相关
+// }, 's');
+
 
 // document.addEventListener('mouseup',redo);
